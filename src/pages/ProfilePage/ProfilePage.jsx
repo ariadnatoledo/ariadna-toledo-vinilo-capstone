c1e62ab2297fbe24fcb52ccd56869b8f501ba744
ariadnatoledo <ariadnatoledo1@gmail.com>
2024-12-03 16:48:02 -0800

    Messages Page was broken, fixed it with the following and followers issue

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -9,30 +9,12 @@ function ProfilePage({ user }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
   const [caption, setCaption] = useState("");
-  const [followers, setFollowers] = useState(0); 
-  const [following, setFollowing] = useState(0); 
+  const [followers, setFollowers ] = useState(0);
+  const [following, setFollowing ] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-
-    const getFriendsData = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3306/friends/${user.userId}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        console.log("Friends API Response:", response.data);
-        setFollowers(response.data.followers);
-        setFollowing(response.data.following);
-      } catch (err) {
-        console.error(err.response?.data?.message || "Error fetching friends data");
-      }
-    };
 
     const getPosts = async () => {
       try {
@@ -48,10 +30,35 @@ function ProfilePage({ user }) {
     };
 
     if (token) {
-      getFriendsData();
       getPosts();
     }
   }, [user.userId]);
+
+useEffect(() => {
+  const token = localStorage.getItem("token");
+
+  const getFriendsData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3306/users/${user.userId}/followers`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Followers API Response:", response.data); 
+      setFollowers(response.data.followers);
+      setFollowing(response.data.following);
+    } catch (err) {
+      console.error("Error fetching followers and following data", err);
+    }
+  };
+
+  if (token) {
+    getFriendsData();
+  }
+}, [user.userId]);
 
   const handleSubmitPost = async (e) => {
     e.preventDefault();
@@ -106,95 +113,97 @@ function ProfilePage({ user }) {
       {user.userId && (
         <>
           <div className="profile-page__header">
-            <img
-              className="profile-page__avatar"
-              src={`http://localhost:3306${user.avatar}`}
-              alt={user.username}
-            />
-            <div className="profile-page__info">
-              <div className="profile-page__username-stats">
-                <h2 className="profile-page__username">{user.username}</h2>
-                <div className="profile-page__follow-stats">
-                  <div className="profile-page__follow-count">
-                    <p>{followers}</p>
-                    <span>Followers</span>
-                  </div>
-                  <div className="profile-page__follow-count">
-                    <p>{following}</p>
-                    <span>Following</span>
-                  </div>
-                </div>
-              </div>
-              <div className="profile-page__upload">
-                <img
-                  src={uploadIcon}
-                  alt="Upload"
-                  className="profile-page__upload-icon"
-                  onClick={() => setIsModalOpen(true)}
-                />
-              </div>
+  <img
+    className="profile-page__avatar"
+    src={`http://localhost:3306${user.avatar}`}
+    alt={user.username}
+  />
+  <div className="profile-page__info">
+    <div className="profile-page__username-stats">
+      <h2 className="profile-page__username">{user.username}</h2>
+      <div className="profile-page__follow-stats">
+        <div className="profile-page__follow-count">
+          <p>{followers}</p>
+          <span>Followers</span>
+        </div>
+        <div className="profile-page__follow-count">
+          <p>{following}</p>
+          <span>Following</span>
+        </div>
+      </div>
 
-              {isModalOpen && (
-                <div className="profile-page__modal">
-                  <div
-                    className="profile-page__modal-backdrop"
-                    onClick={() => setIsModalOpen(false)}
-                  ></div>
-                  <div className="profile-page__modal-content">
-                    <h3>Upload a Photo</h3>
-                    <form
-                      className="profile-page__form"
-                      onSubmit={handleSubmitPost}
-                    >
-                      <div className="profile-page__form-group">
-                        <label
-                          htmlFor="file"
-                          className="profile-page__upload-label"
-                        >
-                          Choose File
-                        </label>
-                        <input
-                          type="file"
-                          id="file"
-                          className="profile-page__file-input"
-                          onChange={(e) => setSelectedFile(e.target.files[0])}
-                        />
-                        {selectedFile && (
-                          <span className="profile-page__file-name">
-                            {selectedFile.name}
-                          </span>
-                        )}
-                      </div>
-                      <textarea
-                        className="profile-page__textarea"
-                        placeholder="Write a caption..."
-                        value={caption}
-                        onChange={(e) => setCaption(e.target.value)}
-                      />
-                      <div className="profile-page__form-actions">
-                        <button
-                          type="submit"
-                          className="profile-page__upload-button"
-                        >
-                          Upload
-                        </button>
-                        <button
-                          type="button"
-                          className="profile-page__modal-close"
-                          onClick={() => setIsModalOpen(false)}
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="profile-page__bio">
-              <p>{user.bio}</p>
-            </div>
+    <div className="profile-page__upload">
+            <img
+              src={uploadIcon}
+              alt="Upload"
+              className="profile-page__upload-icon"
+              onClick={() => setIsModalOpen(true)}
+            />
           </div>
+
+          {isModalOpen && (
+            <div className="profile-page__modal">
+              <div
+                className="profile-page__modal-backdrop"
+                onClick={() => setIsModalOpen(false)}
+              ></div>
+              <div className="profile-page__modal-content">
+                <h3>Upload a Photo</h3>
+                <form
+                  className="profile-page__form"
+                  onSubmit={handleSubmitPost}
+                >
+                  <div className="profile-page__form-group">
+                    <label
+                      htmlFor="file"
+                      className="profile-page__upload-label"
+                    >
+                      Choose File
+                    </label>
+                    <input
+                      type="file"
+                      id="file"
+                      className="profile-page__file-input"
+                      onChange={(e) => setSelectedFile(e.target.files[0])}
+                    />
+                    {selectedFile && (
+                      <span className="profile-page__file-name">
+                        {selectedFile.name}
+                      </span>
+                    )}
+                  </div>
+                  <textarea
+                    className="profile-page__textarea"
+                    placeholder="Write a caption..."
+                    value={caption}
+                    onChange={(e) => setCaption(e.target.value)}
+                  />
+                  <div className="profile-page__form-actions">
+                    <button
+                      type="submit"
+                      className="profile-page__upload-button"
+                    >
+                      Upload
+                    </button>
+                    <button
+                      type="button"
+                      className="profile-page__modal-close"
+                      onClick={() => setIsModalOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          )}
+ </div>
+
+    <div className="profile-page__bio">
+      <p>{user.bio}</p>
+    </div>
+  </div>
+</div>
 
           <div className="profile-page__posts">
             {posts.map((post) => (
@@ -218,5 +227,3 @@ function ProfilePage({ user }) {
 }
 
 export default ProfilePage;
-
-
